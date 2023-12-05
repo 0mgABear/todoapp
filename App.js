c21af6f322640a3fa66b67fc0ccf90b45c3bd41c
0mgABear <yeoyi94@gmail.com>
2023-12-05 16:10:54 +0800

    react native todo app

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleInput = (text) => {
+    console.log(text);
+    setInput(text);
+  };
+
+  const addEntry = () => {
+    setData([...data, input]);
+    setInput("");
+  };
+
+  const renderEntries = () => {
+    return data.map((entry, index) => {
+      return (
+        <View key={index} style={styles.entryContainer}>
+          <Text style={styles.entryText}>
+            {index + 1}. {entry}
+          </Text>
+          <Button onPress={() => deleteEntry(index)} title="Delete" />
+        </View>
+      );
+    });
+  };
+
+  const deleteEntry = (index) => {
+    const newData = [...data.slice(0, index), ...data.slice(index + 1)];
+    setData(newData);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Todo Entry:</Text>
+      <TextInput
+        style={styles.textInput}
+        value={input}
+        onChangeText={handleInput}
+      />
+      <Button title="Add" onPress={addEntry} />
+      <View>{renderEntries()}</View>
     </View>
   );
 }
@@ -13,8 +52,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    width: "90%",
+    height: 40,
+    padding: 5,
+  },
+  entryText: { padding: 10 },
+  entryContainer: {
+    flexDirection: "row",
   },
 });
