3813377d9705ec3e2c8adb4b66f920f15d437f0f
0mgABear <yeoyi94@gmail.com>
2023-12-06 14:15:48 +0800

    added alert and confirmation to delete as well as flatlist

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Alert,
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   const [input, setInput] = useState("");
@@ -23,17 +32,48 @@ export default function App() {
           <Text style={styles.entryText}>
             {index + 1}. {entry}
           </Text>
-          <Button onPress={() => deleteEntry(index)} title="Delete" />
+          <Button
+            title="Delete"
+            onPress={() => {
+              Alert.alert("Todo", "are you sure you want to delete?", [
+                { text: "YES" },
+                { text: "NO" },
+              ]);
+            }}
+          />
         </View>
       );
     });
+  };
+
+  const renderEntry = ({ item, index }) => {
+    return (
+      <View key={index} style={styles.entryContainer}>
+        <Text style={styles.entryText}>
+          {index + 1}. {item}
+        </Text>
+        <Button
+          onPress={() => {
+            Alert.alert("Todo", "Are you sure you want to delete?", [
+              {
+                text: "YES",
+                onPress: () => {
+                  deleteEntry(index);
+                },
+              },
+              { text: "NO" },
+            ]);
+          }}
+          title="Delete"
+        />
+      </View>
+    );
   };
 
   const deleteEntry = (index) => {
     const newData = [...data.slice(0, index), ...data.slice(index + 1)];
     setData(newData);
   };
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -44,7 +84,12 @@ export default function App() {
         onChangeText={handleInput}
       />
       <Button title="Add" onPress={addEntry} />
-      <View>{renderEntries()}</View>
+      <FlatList
+        style={styles.scrollView}
+        data={data}
+        renderItem={renderEntry}
+      />
+      {/* <ScrollView style={styles.scrollView}>{renderEntries()}</ScrollView> */}
     </View>
   );
 }
@@ -65,5 +110,9 @@ const styles = StyleSheet.create({
   entryText: { padding: 10 },
   entryContainer: {
     flexDirection: "row",
+  },
+  scrollView: {
+    width: "80%",
+    maxHeight: "60%",
   },
 });
